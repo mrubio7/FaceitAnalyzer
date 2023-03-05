f082549529b20c3f73ef6a0bcad6bce9c2536124
mrubio7 <manuel.rubio.arias@gmail.com>
2023-03-06 00:40:32 +0100

    loader

--- public/js/vue/match-search.vue
+++ public/js/vue/match-search.vue
@@ -7,15 +7,63 @@
 				<button type="button" @click="forceRerender">Go</button>
 			</form>
 		</div>
-		<div v-if="match != null">
-			<MatchResult :match="match" :key="componentKey" />
+
+		<div v-if="loading == false">
+
+			<div v-if="match != null">
+				<MatchResult :match="match" :key="componentKey" />
+			</div>
+			<div v-else>
+				<div style="margin-top: 100px; color: #232323;" class="flex-column center">
+					<h1>Busca una partida</h1>
+					<img src="/images/undraw_personal_goals_re_iow7.svg" width="500" style="mix-blend-mode: overlay;">
+				</div>
+			</div>
+
 		</div>
 		<div v-else>
-			<div style="margin-top: 100px; color: #232323;" class="flex-column center">
-				<h1>Busca una partida</h1>
-				<img src="/images/undraw_personal_goals_re_iow7.svg" width="500" style="mix-blend-mode: overlay;">
+
+			<div style="margin-top: 200px; color: #232323;" class="flex-column center">
+				<main>
+					<div class="dank-ass-loader">
+						<div class="row">
+							<div class="arrow up outer outer-18"></div>
+							<div class="arrow down outer outer-17"></div>
+							<div class="arrow up outer outer-16"></div>
+							<div class="arrow down outer outer-15"></div>
+							<div class="arrow up outer outer-14"></div>
+						</div>
+						<div class="row">
+							<div class="arrow up outer outer-1"></div>
+							<div class="arrow down outer outer-2"></div>
+							<div class="arrow up inner inner-6"></div>
+							<div class="arrow down inner inner-5"></div>
+							<div class="arrow up inner inner-4"></div>
+							<div class="arrow down outer outer-13"></div>
+							<div class="arrow up outer outer-12"></div>
+						</div>
+						<div class="row">
+							<div class="arrow down outer outer-3"></div>
+							<div class="arrow up outer outer-4"></div>
+							<div class="arrow down inner inner-1"></div>
+							<div class="arrow up inner inner-2"></div>
+							<div class="arrow down inner inner-3"></div>
+							<div class="arrow up outer outer-11"></div>
+							<div class="arrow down outer outer-10"></div>
+						</div>
+						<div class="row">
+							<div class="arrow down outer outer-5"></div>
+							<div class="arrow up outer outer-6"></div>
+							<div class="arrow down outer outer-7"></div>
+							<div class="arrow up outer outer-8"></div>
+							<div class="arrow down outer outer-9"></div>
+						</div>
+					</div>
+					</main>
 			</div>
+
 		</div>
+			
 	</div>
 </template>
 
@@ -32,10 +80,14 @@
 				matchId: "",
 				matchIdInput: "",
 				match: null,
+				loading: false,
 			};
 		},
 		methods: {
 			async forceRerender() {
+				this.loading = true;
+				await this.$nextTick();
+
 				const re = new RegExp("[a-zA-Z0-9-]*$")
 				const matchId = re.exec(this.matchIdInput)[0]
 				
@@ -43,7 +95,9 @@
 				const res = await fetch(`/analyze?q=${matchId}`);
 				const finalRes = await res.json();
 				this.match = Object.assign({}, finalRes);
+				this.loading = false;
 				await this.$nextTick(); // Espera a que Vue termine de actualizar la propiedad "matchId".
+
 			},
 		}
 	}
@@ -110,4 +164,118 @@
 		width: 1px;
 		overflow: hidden;
 	}
+
+
+
+
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.dank-ass-loader {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.dank-ass-loader .row {
+		display: flex;
+	}
+	.arrow {
+		width: 0;
+		height: 0;
+		margin: 0 -6px;
+		border-left: 12px solid transparent;
+		border-right: 12px solid transparent;
+		border-bottom: 21.6px solid #fd7000;
+		animation: blink 1s infinite;
+		filter: drop-shadow(0 0 18px #fd7000);
+	}
+	.arrow.down {
+		transform: rotate(180deg);
+	}
+	.arrow.outer-1 {
+		animation-delay: -0.0555555556s;
+	}
+	.arrow.outer-2 {
+		animation-delay: -0.1111111111s;
+	}
+	.arrow.outer-3 {
+		animation-delay: -0.1666666667s;
+	}
+	.arrow.outer-4 {
+		animation-delay: -0.2222222222s;
+	}
+	.arrow.outer-5 {
+		animation-delay: -0.2777777778s;
+	}
+	.arrow.outer-6 {
+		animation-delay: -0.3333333333s;
+	}
+	.arrow.outer-7 {
+		animation-delay: -0.3888888889s;
+	}
+	.arrow.outer-8 {
+		animation-delay: -0.4444444444s;
+	}
+	.arrow.outer-9 {
+		animation-delay: -0.5s;
+	}
+	.arrow.outer-10 {
+		animation-delay: -0.5555555556s;
+	}
+	.arrow.outer-11 {
+		animation-delay: -0.6111111111s;
+	}
+	.arrow.outer-12 {
+		animation-delay: -0.6666666667s;
+	}
+	.arrow.outer-13 {
+		animation-delay: -0.7222222222s;
+	}
+	.arrow.outer-14 {
+		animation-delay: -0.7777777778s;
+	}
+	.arrow.outer-15 {
+		animation-delay: -0.8333333333s;
+	}
+	.arrow.outer-16 {
+		animation-delay: -0.8888888889s;
+	}
+	.arrow.outer-17 {
+		animation-delay: -0.9444444444s;
+	}
+	.arrow.outer-18 {
+		animation-delay: -1s;
+	}
+	.arrow.inner-1 {
+		animation-delay: -0.1666666667s;
+	}
+	.arrow.inner-2 {
+		animation-delay: -0.3333333333s;
+	}
+	.arrow.inner-3 {
+		animation-delay: -0.5s;
+	}
+	.arrow.inner-4 {
+		animation-delay: -0.6666666667s;
+	}
+	.arrow.inner-5 {
+		animation-delay: -0.8333333333s;
+	}
+	.arrow.inner-6 {
+		animation-delay: -1s;
+	}
+	@keyframes blink {
+		0% {
+			opacity: 0.1;
+		}
+		30% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 0.1;
+		}
+	}
+
 </style>
