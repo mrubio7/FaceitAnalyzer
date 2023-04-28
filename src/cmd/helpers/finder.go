5d8e38500efb22f97d2f6fbdc0dc7fabaa8fcd0c
mrubio <manuel.rubio.arias@gmail.com>
2023-04-28 08:27:36 +0200

    Update finder.go

--- src/cmd/helpers/finder.go
+++ src/cmd/helpers/finder.go
@@ -32,7 +32,7 @@ func (f finder) FindMatchesStats7Days(playerId string) []models.Match {
 	// 	log.Fatal(err)
 	// }
 
-	curl := exec.Command("curl", "--header", "Host:api.faceit.com", "--header", "Authorization:'Bearer 3a839030-b132-4733-8815-da9a478e835a'", apiurls_constants.GetMatchList1+playerId+apiurls_constants.GetMatchList2+"20")
+	curl := exec.Command("curl", "--header", "Host:api.faceit.com", "--header", "Authorization:'Bearer 3a839030-b132-4733-8815-da9a478e835b'", apiurls_constants.GetMatchList1+playerId+apiurls_constants.GetMatchList2+"20")
 	out, err := curl.Output()
 	if err != nil {
 		fmt.Println("erorr", err)
@@ -62,7 +62,7 @@ func (f finder) FindMatch(matchId string) models.Match {
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
-	curl := exec.Command("curl", "--header", "Host:api.faceit.com", "--header", "Authorization:'Bearer 3a839030-b132-4733-8815-da9a478e835a'", apiurls_constants.GetMatchStats+matchId)
+	curl := exec.Command("curl", "--header", "Host:api.faceit.com", "--header", "Authorization:'Bearer 3a839030-b132-4733-8815-da9a478e835b'", apiurls_constants.GetMatchStats+matchId)
 	out, err := curl.Output()
 	if err != nil {
 		fmt.Println("erorr", err)
@@ -77,11 +77,11 @@ func (f finder) FindLiveMatch(matchId string) models.LiveMatch {
 	// req, _ := http.NewRequest("GET", apiurls_constants.GetMatchLive+matchId, nil)
 
 	// req.Host = "api.faceit.com"
-	// req.Header.Add("Authorization", "Bearer 3a839030-b132-4733-8815-da9a478e835a")
+	// req.Header.Add("Authorization", "Bearer 3a839030-b132-4733-8815-da9a478e835b")
 
 	// response, err := http.DefaultClient.Do(req)
 
-	curl := exec.Command("curl", "--header", "Host:api.faceit.com", "--header", "Authorization:'Bearer 3a839030-b132-4733-8815-da9a478e835a'", apiurls_constants.GetMatchLive+matchId)
+	curl := exec.Command("curl", "--header", "Host:api.faceit.com", "--header", "Authorization:'Bearer 3a839030-b132-4733-8815-da9a478e835b'", apiurls_constants.GetMatchLive+matchId)
 	out, err := curl.Output()
 	if err != nil {
 		fmt.Println("erorr", err)
